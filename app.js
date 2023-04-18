972cdb19deaf7ae0b7da004a0ef3c7d043d86c73
Bhanu9Prakash <f20180317@pilani.bits-pilani.ac.in>
2023-04-18 20:30:50 +0530

    commit message

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const PORT = 4002;
 
 // middleware
 app.use(express.json());
-app.use(cors((origin = ["http://localhost:3000", "https://flask-production-8563.up.railway.app/"])));
+app.use(cors((origin = ["http://localhost:3000", "https://web-production-de1e.up.railway.app/"])));
 app.use(router);
 
 app.listen(PORT, () => {
